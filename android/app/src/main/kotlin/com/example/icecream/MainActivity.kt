499a33723e1ba5367413932477cd8ce56dc2ecf4
Shedrack Kitomari <shedrack.kitomari47@gmail.com>
2021-08-06 21:04:56 +0300

    First commit

---
+++ android/app/src/main/kotlin/com/example/icecream/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.icecream
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Homepage(),
+    );
+  }
+}
+
+
+class Homepage extends StatefulWidget {
+
+  @override
+  _HomepageState createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home")
+      ),
+      body: Center(
+        child: Text("Karibuu")
+      ),
+    );
+  }
+}
